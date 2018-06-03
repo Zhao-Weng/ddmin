e24cbaeeacb2901a6fefcfadf79a0903ffa173d4
klissancall <wxy449134322@gmail.com>
2018-06-03 00:32:11 -0700

    Add files via upload
    Rewrite makeSubset, makeComplement to add file input. Add handleInput to read file and executeTest to run the program.

--- ddmin.py
+++ ddmin.py
@@ -1,102 +1,147 @@
-import pdb
+import sys
+import subprocess
 
 
 class Result:
-	Pass = 1
-	Fail = 2
-	Unresolved = 3
+    Pass = 1
+    Fail = 2
+    Unresolved = 3
 
 
 class BreakoutException(Exception):
-	def __init__(self, message):
-		self.message = message
+    def __init__(self, message):
+        self.message = message
 
 
 def Minimize(data, f):
-	if (f([]) == Result.Fail):
-		return []
-	if (f(data) == Result.Pass):
-		raise ValueError('ddmin: function must fail on data')
-	return ddmin(data, f, 2)
+    if (f([]) == Result.Fail):
+        return []
+    if (f(data) == Result.Pass):
+        raise ValueError('ddmin: function must fail on data')
+    return ddmin(data, f, 2)
 
 
 def ddmin(data, f, granularity):
-	while (len(data) >= 2):
-		try:
-	
-			subsets = makeSubsets(data, granularity)
-			# pdb.set_trace()
-			for subset in subsets:
-				#print(subset)
-				if (f(subset) == Result.Fail):
-					data = subset
-					granularity = 2
-					raise BreakoutException('continue while loop')
-			b = [0 for i in range(len(data))]
-			for i in range(len(subsets)):
-				complement = makeComplement(subsets, i, b[:0])
-				if (f(complement) == Result.Fail):
-					granularity -= 1
-					if (granularity < 2):
-						granularity = 2
-					data = complement
-					raise BreakoutException('continue while loop')
-			if (granularity == len(data)):
-				return data
-			granularity *= 2
-			if (granularity > len(data)):
-				granularity = len(data)
-			# print('granunarity: {0}\n'.format(granularity))
-		except BreakoutException:
-			# print('granunarity: {0}\n'.format(granularity))
-			pass
-	return data
+    #whole = data
+    #length = len(data)
+    while (len(data) >= 2):
+        try:
+            subsets = makeSubsets(data, granularity)
+            # pdb.set_trace()
+            for subset in subsets:
+                #print(subset)
+                if (f(subset) == Result.Fail):
+                    data = subset
+                    granularity = 2
+                    raise BreakoutException('continue while loop')
+            for i in range(len(subsets)):
+                complement = makeComplement(subsets, i, data)
+                if (f(complement) == Result.Fail):
+                    granularity -= 1
+                    if (granularity < 2):
+                        granularity = 2
+                    data = complement
+                    raise BreakoutException('continue while loop')
+            if (granularity == len(data)):
+                return data
+            granularity *= 2
+            if (granularity > len(data)):
+                granularity = len(data)
+            # print('granunarity: {0}\n'.format(granularity))
+        except BreakoutException:
+            # print('granunarity: {0}\n'.format(granularity))
+            pass
+    return data
 
 
 def makeSubsets(s, n):
-	ret = []
-	while(n > 0):
-		i = len(s) // n
-		copyS = s
-		s = copyS[i:]
-		ret.append(copyS[:i])
-		n -= 1      # bug!!!
-	# print('ret is:{0}\n'.format(ret))
-	return ret
+    ret = []
+    while(n > 0):
+        i = len(s) // n
+        copyS = s
+        s = copyS[i:]
+        ret.append("".join(copyS[:i]))
+        n -= 1
+    return ret
 
 
-def makeComplement(subsets, n, b):
-	for i in range(len(subsets)):
-		s = subsets[i]
-		if (i == n):
-			continue
-		b += s
-	# print('b is:{0}\n'.format(b))
-	return b
+def makeComplement(subsets, n, data):
+    s = list(subsets[n])
+    l = len(data)
+    i = 0
+    j = 0
+    re = []
+    while(j < l):
+        #print(i)
+        if i == len(s):
+            re.append(data[j])
+            j += 1
+            continue
+        if (s[i] != data[j]):
+            re.append(data[j])
+            j += 1
+        else:
+            j += 1
+            i += 1
+
+    return "".join(re)
 
 
 def f(d):
-	seen1, seen2, seen3 = False, False, False
-	for v in d:
-		if (v == 1):
-			seen1 = True
-		if (v == 7):
-			seen2 = True
-		if (v == 8):
-			seen3 = True
-	if (seen1 and seen2 and seen3):
-		return Result.Fail
-	else:
-		return Result.Pass
+    executetest(d)
+    #seen1, seen2, seen3 = False, False, False
+    #for v in d:
+        #if (v == 1):
+            #seen1 = True
+        #if (v == 7):
+            #seen2 = True
+       # if (v == 8):
+            #seen3 = True
+    #if (seen1 and seen2 and seen3):
+        #return Result.Fail
+    #else:
+        #return Result.Pass
 
 
+#??????????
 def ExampleMinimize():
-	data = [1, 2, 3, 4, 5, 6, 7, 8]
-	m = Minimize(data, f)
-	print(m)
+    code = "".join(handleInput())
+
+    #data = [1, 2, 3, 4, 5, 6, 7, 8]
+    m = Minimize(code, f)
+    #print(m)
+
+
+def handleInput():
+    filename = sys.argv[1]
+    if filename is not None:
+        c = 1
+        f = open(filename, "r")
+        char = f.read(1)
+        sub = []
+        while char:
+            sub.append(char)
+            char = f.read(1)
+            c += 1
+        #print(sub)
+        #print(c)
+        f.close()
+    return sub
+
+def executetest(code):
+    # arguments = "t.py".encode('utf-8')
+    # filename = "handleInput.py"
+    # result = subprocess.run(["python", filename, "t.py"], stderr=subprocess.PIPE)
+    # print(result.stdout.decode('utf-8'))
+    try:
+        exec(code)
+    except Exception:
+        return Result.Fail
+
+    return Result.Pass
 
 
 if __name__ == '__main__':
-	ExampleMinimize()
+    ExampleMinimize()
 
 
