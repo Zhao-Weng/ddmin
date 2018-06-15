2c484026f71312a54c865c42dc856a470be97984
Zhaowei Tan <tanzw94@gmail.com>
2018-06-15 16:07:43 -0700

    add -r test case

---
+++ testcase/module/fibo.py
@@ -0,0 +1,9 @@
+def fib(n):    # write Fibonacci series up to n
+    n = 10
+    sum1 = 0
+    for i in range(1, n + 1):
+        sum1 = sum1 + (1 / i)
+    assert (i == 11), "gg"
+    print("The sum of series is", round(sum1, 2))
+
+
---
+++ testcase/module/fibo_main.py
@@ -0,0 +1,10 @@
+import sys
+import os.path
+
+sys.path.append('/Users/klissan/Desktop/d/testcase/module')
+sys.path.append('/Users/ZhaoweiTan/Dropbox/Phd/courses/239/ddmin/testcase/module')
+# print (sys.path)
+
+import fibo
+
+print (fibo.fib(100))
